44078c40bb0c8c7a2f8fc369f12130441396d88f
Jacobus Meintjes <jacobus.meintjes@gmail.com>
2018-02-01 17:18:10 +0200

    Menu Loading dropdown list
    Menu loading dropdown list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,14 @@ import { TasksModule } from './tasks/tasks.module';
 import { MenuService } from './app.services';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './common/menu/menu.component';
+import { HeaderComponent } from './common/header/header.component';
+import { MenuItemComponent } from './common/menu/menuitem.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MenuComponent, MenuItemComponent, HeaderComponent
   ],
   imports: [
     BrowserModule,
